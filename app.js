abd836f6e4c877db26687a89309b6529a74063a5
kathe <kathemica@gmail.com>
2021-03-25 22:24:07 -0300

    Cleaning code

--- app.js
+++ app.js
@@ -6,59 +6,44 @@ require('dotenv').config();
 
 import fs from 'fs';
 import colors from 'colors';
-import cliProgress from 'cli-progress';
-const _progress = require('cli-progress');
+import _progress from 'cli-progress';
+import { randomInt, random} from './assets/supportFunctions.js'
 
-// import { NODE_ENVIRONMENT } from './config/config.js';
-import { randomInt, random, sleep } from './assets/supportFunctions.js'
-
-// console.log(`NODE_ENV=${NODE_ENVIRONMENT}`);
-
-const main = async () => {      
-  
-  // console.log(process.env);
-  
-  const ca = [fs.readFileSync(process.env.CA_CERT)];    
-  const key = fs.readFileSync(process.env.KEY_CERT);
-  const cert = fs.readFileSync(process.env.PEM_CERT);
-  const sslPass = process.env.CA_TOKEN;
-  const replicaSet= process.env.REPLICASET;
-  const serverName= process.env.HOST;
-  const port= process.env.SERVICE_PORT;
-  const authMechanism= process.env.AUTHMECANISM;    
-  const readPreference = process.env.READPREFERENCE;
-  const isSSL = process.env.USE_SSL;
-  
-     const url = `mongodb://10.0.0.12:27017/?authMechanism=MONGODB-X509&replicaSet=my-replica-set&readPreference=primary&authSource=%24external&appname=MongoDB%20Compass&ssl=true&authSource=$external`;
-  // const url = `mongodb://${serverName}:${port}/?authMechanism=${authMechanism}&replicaSet=${replicaSet}&readPreference=${readPreference}&authSource=%24external&appname=mongodbReplicaset&ssl=${isSSL}`;
+const main = async () => {   
+  const pEnv = process.env;       
+  const ca = [fs.readFileSync(pEnv.CA_CERT)];    
+  const key = fs.readFileSync(pEnv.KEY_CERT);
+  const cert = fs.readFileSync(pEnv.PEM_CERT);  
+      
+  const url = `mongodb://${pEnv.HOST}:${pEnv.SERVICE_PORT}/?authMechanism=${pEnv.AUTHMECANISM}&replicaSet=${pEnv.REPLICASET}&readPreference=${pEnv.READPREFERENCE}&authSource=%24external&appname=mongodbReplicaset&ssl=${JSON.parse(pEnv.USE_SSL.toLowerCase())}`;
 
   let options = {           
       useNewUrlParser: true,
       poolSize: 15,                        
-      ssl: true,
+      ssl: JSON.parse(pEnv.USE_SSL.toLowerCase()),
       sslValidate: false,
       sslCA: ca,
       sslKey: key,
       sslCert: cert,
-      sslPass,
+      sslPass: pEnv.CA_TOKEN,
       tlsAllowInvalidHostnames: true,
       useUnifiedTopology: true
   };
   
   try{    
     console.clear();
+
     console.log(`${'Creating connection'.green}\n` );         
     const client= MongoClient.connect(url, options);
     console.log(`${'\nIn progress...'.blue} \nAwaiting for being connected and start the process\n`)
     
     const db = (await client).db('iot');    
-    db ? console.log(`${'Connected'.green} to DB ${'IoT'.yellow}\nMessage: Trying to populate data in ${'devices'.blue} collation.\n` ) : new error("Error con la base de datos");
+    db ? console.log(`${'Connected'.green} to DB ${'IoT'.cyan}\nMessage: Trying to populate data in ${'devices'.blue} collation.\n` ) : new error("Error con la base de datos");
     
-    const MAX_INTERATIONS = process.env.MAX_INTERATIONS;
+    const MAX_INTERATIONS = pEnv.MAX_INTERATIONS;
     
     console.log(`Populating ${MAX_INTERATIONS} devices, be patient.\n`)
-    
-    // const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+      
     const progressBar = new _progress.Bar({
         // blue bar, reset styles after bar element
         format: 'progress [\u001b[34m{bar}\u001b[0m] {percentage}% | ETA: {eta}s | {value}/{total} | Sensor ID: {speed}',
@@ -82,12 +67,14 @@ const main = async () => {
     // stop the progress bar
     progressBar.stop();
 
-    console.log(`\n${'That\'s it,'.blue} Good bye!!\n\n\n`)
-    console.log(`${'Thank you for using my software'.green} Eng. Katherine Aguirre !!\n`)
-    process.exit();                                    
+    console.log(`\n${'That\'s it,'.blue} Good bye!!\n\n\n`);
+    console.log(`${'Thank you for using my software'.green} Eng. Katherine Aguirre !!\n`);
+
+    process.exit();            
+
   }catch(e){
       console.log(`${'SOMETHING WENT WRONG'.red} we're in trouble, I got this: ${e}\n`);  
-      console.log(`${'Closing app...'.blue} Good bye!!\n` )
+      console.log(`${'Closing app...'.blue} Good bye!!\n` );
       process.exit();      
   }    
 }
