41a48da00e4c524a47e6a7bdf2bfe5dc2357f5c3
kathe <kathemica@gmail.com>
2021-03-27 19:04:41 -0300

    Fixing some issues with file paths

--- ssl/scripts/prod_env/generateCertificates.sh
+++ ssl/scripts/prod_env/generateCertificates.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-logText=" ..... "
+TXT_LOG =" ..... "
 CONFS_FILES_DIR="./node_cnf/"
 
 #-------------------------------------------------------------------
@@ -50,10 +50,9 @@ Client_CN_CNF="${CONFS_FILES_DIR}client_CN.cnf"
 # Orden de los parámetros
 # DIR         ----- $1 - $X_DIR
 # FILE        ----- $2 - $FILEVAR_NAME
-move_files() {
-    tree
+move_files() {    
     mkdir $1 2> /dev/null
-    printf "Moviendo $2 A ./$1/$2 $logText \n"
+    printf "Moviendo $2 A ./$1/$2 $TXT_LOG  \n"
     mv ./$2 ./$1/$2
 }
 
@@ -61,10 +60,9 @@ move_files() {
 # Orden de los parámetros
 # DIR         ----- $1 - $X_DIR
 # FILE        ----- $2 - $FILEVAR_NAME
-copy_files() {
-    tree
+copy_files() {    
     mkdir $1 2> /dev/null
-    printf "Copiando $2 A ./$1/$2 $logText \n"
+    printf "Copiando $2 A ./$1/$2 $TXT_LOG  \n"
     cp ./$2 ./$1/$2
 }
 
@@ -78,20 +76,20 @@ copy_files() {
 # MDB CA .key file  ----- $6 - $Server_KEY
 # NODE .crt file  ----- $7 - $MDB_NODEX_CRT
 gen_replicakeycerts(){
-    printf "\nGenerando certificados de $1 $logText \n"
+    printf "\nGenerando certificados de $1 $TXT_LOG  \n"
     
     mkdir $1 2> /dev/null
 
-    printf "nGenerando $1 - archivos .KEY y .CSR $logText\n"
+    printf "nGenerando $1 - archivos .KEY y .CSR $TXT_LOG \n"
     
     openssl genrsa -des3 -out $2 -passout pass:"$PASS_PHRASE_CA" 4096
     
-    printf "\nGenerando $1 - archivo .CRT $logText\n"
+    printf "\nGenerando $1 - archivo .CRT $TXT_LOG \n"
     
     openssl req -new -config $4 -key $2 -passin pass:"$PASS_PHRASE_CA" -out $3 -config $4
     openssl x509 -req -days 365 -in $3 -CA $5 -CAkey $6 -CAcreateserial -passin pass:"$PASS_PHRASE_CA" -out $7
     
-    printf "\nGenerando $1 - archivo .PEM $logText\n"
+    printf "\nGenerando $1 - archivo .PEM $TXT_LOG \n"
     
     cat $2 $7 > $FINAL_KEYCERT_PEM
     
@@ -101,40 +99,43 @@ gen_replicakeycerts(){
     move_files $1 $FINAL_KEYCERT_PEM
     copy_files $1 $5
     
-    printf "\FINALIZADO... $1 $logText\n"
+    printf "\FINALIZADO... $1 $TXT_LOG \n"
 }
 
 openssl rand -out .rnd -hex 256
-printf "INICIANDO SCRIPT $logText\n\n"
+printf "INICIANDO SCRIPT $TXT_LOG \n\n"
 
 openssl genrsa -des3 -out $Server_KEY -passout pass:"$PASS_PHRASE_CA" 4096
-printf "Root CA .key OK $logText \n"
+printf "Root CA .key OK $TXT_LOG  \n"
 
 openssl req -x509 -new -key $Server_KEY -sha256 -passin pass:"$PASS_PHRASE_CA" -days 720 -out $Server_CRT -config $Server_CNF
-printf "Root CA .crt OK $logText \n"
+printf "Root CA .crt OK $TXT_LOG  \n"
 
-printf "FINALIZADO CERTIFICADO CA $logText \n"
+printf "FINALIZADO CERTIFICADO CA $TXT_LOG  \n"
 
 gen_replicakeycerts $Node01_DIR $Node01_KEY $Node01_CSR $Node01_CNF $Server_CRT $Server_KEY $Node01_CRT
 gen_replicakeycerts $Node02_DIR $Node02_KEY $Node02_CSR $Node02_CNF $Server_CRT $Server_KEY $Node02_CRT
 gen_replicakeycerts $NodeArbiter_DIR $NodeArbiter_KEY $NodeArbiter_CSR $NodeArbiter_CNF $Server_CRT $Server_KEY $NodeArbiter_CRT
 
-printf "Generando certificados de acceso del cliente: .key y .cert $logText \n"
+printf "Generando certificados de acceso del cliente: .key y .cert $TXT_LOG  \n"
 openssl req -new -out $Client_CSR -keyout $Client_KEY -passout pass:"$PASS_PHRASE_CA" -config $Client_CN_CNF
 
-printf "Firmando certificados de acceso del cliente$logText \n"
+printf "Firmando certificados de acceso del cliente $TXT_LOG  \n"
 openssl x509 -req -in $Client_CSR -CA $Server_CRT -CAkey $Server_KEY -passin pass:"$PASS_PHRASE_CA" -out $Client_CRT
 
-printf "Generando archivo .PEM del cliente $logText \n"
+printf "Generando archivo .PEM del cliente $TXT_LOG  \n"
 
 cat $Client_KEY $Client_CRT > $Client_PEM
+
 move_files $Client_DIR $Client_CSR
 move_files $Client_DIR $Client_KEY
 move_files $Client_DIR $Client_CRT
 move_files $Client_DIR $Client_PEM
+
 copy_files $Client_DIR $Server_CRT
+
 move_files $Server_DIR $Server_KEY
 move_files $Server_DIR $Server_CRT
 move_files $Server_DIR $Server_SRL
 
-printf "FINALIZADO $1 $logText\n"
+printf "FINALIZADO $1 $TXT_LOG \n"
